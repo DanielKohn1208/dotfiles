d67365ef51d1a2d827ec103613b22775df5c25bc
DanielKohn1208 <danielmkohn1208@gmail.com>
2022-12-25 12:54:41 -0500

    fixing some neovim stuff

--- .config/nvim/lua/lsp/lsp-config.lua
+++ .config/nvim/lua/lsp/lsp-config.lua
@@ -1,10 +1,10 @@
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
@@ -45,26 +45,28 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", "<cmd>lua require'telescope.builtin'.lsp_implementations{}<cr>", bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", "<cmd>lua require'telescope.builtin'.lsp_references{}<cr>", bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
 end
 
 local lsp_flags = {
@@ -72,7 +74,7 @@ local lsp_flags = {
 	debounce_text_changes = 150,
 }
 
-require('lspconfig')['sumneko_lua'].setup {
+require("lspconfig")["sumneko_lua"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
 	settings = {
@@ -88,38 +90,35 @@ require('lspconfig')['sumneko_lua'].setup {
 			},
 		},
 	},
-
-}
-require('lspconfig')['cssls'].setup {
+})
+require("lspconfig")["cssls"].setup({
 	on_attach = on_attach,
-
-}
-require('lspconfig')['marksman'].setup {
+})
+require("lspconfig")["marksman"].setup({
 	on_attach = on_attach,
-}
+})
 
-require('lspconfig').jdtls.setup{
+require("lspconfig").jdtls.setup({
 	on_attach = on_attach,
-}
+})
 
-require('lspconfig')['html'].setup {
+require("lspconfig")["html"].setup({
 	on_attach = on_attach,
-}
-require('lspconfig')['tsserver'].setup {
+})
+require("lspconfig")["tsserver"].setup({
 	on_attach = on_attach,
 	-- root_dir = function() return vim.loop.cwd() end      -- run lsp for javascript in any directory
+})
 
-}
-
-require('lspconfig')['pyright'].setup {
+require("lspconfig")["pyright"].setup({
 	on_attach = on_attach,
 	settings = {
 		python = {
 			analysis = {
-				autoSearchPaths = true, 
+				autoSearchPaths = true,
 				typeCheckingMode = "off",
 				useLibraryCodeForTypes = true,
-			}
-		}
+			},
+		},
 	},
-}
+})
